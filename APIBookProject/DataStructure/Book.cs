af287cc387d477229e6ddc7938d8b8eafa4a8630
MartinTT00 <61382773+MartinTT00@users.noreply.github.com>
2021-01-21 19:41:13 +0200

    Creating Models: Book and Language

---
+++ APIBookProject/DataStructure/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure
+{
+    public class Book : BaseModel
+    {
+        public int YearOfPublishment { get; set; }
+    }
+}
