7650c7bd98e94b897fa471fc505cc4986c9fb8c5
MartinTT00 <61382773+MartinTT00@users.noreply.github.com>
2021-01-22 20:03:45 +0200

    Create Author's Repository and Controller with GetAuthors method

---
+++ APIBookProject/APIBookProject/Controllers/AuthorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess;
+using DataStructure;
+using DataAccess.Repositories;
+
+namespace APIBookProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private AuthorRepository authorRepository;
+        public AuthorController(AuthorRepository authorRepository)
+        {
+           this.authorRepository = authorRepository;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Author> authors = authorRepository.GetAuthors();
+            return Ok(authors);
+        }
+    }
+}
---
+++ APIBookProject/DataAccess/Repositories/AuthorRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataStructure;
+
+namespace DataAccess.Repositories
+{
+    public class AuthorRepository
+    {
+        private AppDbContext appDbContext;
+        public AuthorRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public List<Author> GetAuthors()
+        {
+            List<Author> authors = new List<Author>();
+            authors = appDbContext.Authors.ToList();
+            return authors;
+        }
+
+    }
+}
