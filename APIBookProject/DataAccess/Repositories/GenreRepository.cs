1b1e9de401048d42624832b434f9e171dcecbc0d
MartinTT00 <61382773+MartinTT00@users.noreply.github.com>
2021-01-23 15:16:28 +0200

    Create Genre Repository

---
+++ APIBookProject/DataAccess/Repositories/GenreRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataStructure;
+
+namespace DataAccess.Repositories
+{
+    public class GenreRepository
+    {
+        private AppDbContext appDbContext;
+        public GenreRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public List<Genre> GetGenres()
+        {
+            List<Genre> genres = new List<Genre>();
+            genres = appDbContext.Genres.ToList();
+            return genres;
+        }
+
+
+        public Genre PostGenre(Genre genre)
+        {
+            appDbContext.Genres.Add(genre);
+            Save();
+            return genre;
+        }
+
+
+        public void Save()
+        {
+            appDbContext.SaveChanges();
+        }
+
+        public Genre GetByID(int id)
+        {
+            Genre genre = new Genre();
+            genre = appDbContext.Genres.Find(id);
+            return genre;
+        }
+
+
+
+        public void PutGenre(int id)
+        {
+            Genre genre = new Genre();
+            genre = GetByID(id);
+            appDbContext.Genres.Update(genre);
+            Save();
+        }
+
+        public void DeleteGenre(int id)
+        {
+            Genre genre = new Genre();
+            genre = GetByID(id);
+            appDbContext.Genres.Remove(genre);
+            Save();
+
+        }
+
+    }
+}
